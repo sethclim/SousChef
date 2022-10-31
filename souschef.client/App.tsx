8049c846e7da3383e48e0d95fa1358ca7e72283a
sethclim <sethclim@gmail.com>
2022-10-31 00:36:10 -0400

    Added Design System

--- souschef.client/App.tsx
+++ souschef.client/App.tsx
@@ -1,7 +1,25 @@
+import React, {useEffect, useState} from 'react';
 import RootNavigator from './src/navigation';
+import {useColorScheme} from 'react-native';
+
+import {theme, darkTheme} from './src/styles/theme';
+
+const ThemeContext = React.createContext({});
 
 const App = () => {
-  return <RootNavigator />;
+  const [darkMode, setDarkMode] = useState(false);
+
+  let dark = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    setDarkMode(dark);
+  }, [dark]);
+
+  return (
+    <ThemeContext.Provider value={darkMode ? darkTheme : theme}>
+      <RootNavigator />;
+    </ThemeContext.Provider>
+  );
 };
 
 export default App;
