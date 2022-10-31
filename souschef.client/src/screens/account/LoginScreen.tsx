5294be26a1d94785450125f01c70f037e17ace1f
sethclim <sethclim@gmail.com>
2022-10-30 22:29:05 -0400

    Check Against Fake Data for Signin

--- souschef.client/src/screens/account/LoginScreen.tsx
+++ souschef.client/src/screens/account/LoginScreen.tsx
@@ -3,24 +3,40 @@ import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
 import {LoginScreenNavigationProp} from '../../navigation/types';
 
 const LoginScreen = ({navigation, route}: LoginScreenNavigationProp) => {
-  const [email, onChangeEmail] = React.useState('Email');
-  const [password, onChangePassword] = React.useState('Password');
+  const [email, onChangeEmail] = React.useState('');
+  const [password, onChangePassword] = React.useState('');
+  const [error, setError] = React.useState('');
+
+  let fakeLogin = {
+    pass: '123',
+    email: '123@gmail.com',
+  };
+
+  const login = () => {
+    if (email === fakeLogin.email && password === fakeLogin.pass) {
+      navigation.navigate('HomeScreen');
+    } else {
+      setError('wrong email or password!');
+    }
+  };
 
   return (
     <View style={{flex: 1, paddingTop: 10}}>
       <Text>Login</Text>
+      <Text>{error}</Text>
       <TextInput
         style={styles.input}
         onChangeText={onChangeEmail}
         value={email}
+        placeholder={'Email'}
       />
       <TextInput
         style={styles.input}
         onChangeText={onChangePassword}
         value={password}
+        placeholder={'Password'}
       />
-
-      <Button title="Login" onPress={() => navigation.navigate('HomeScreen')} />
+      <Button title="Login" onPress={() => login()} />
     </View>
   );
 };
