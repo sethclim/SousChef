4ed2815f30147ad64dcc67f7590ee5c76394a868
sethclim <sethclim@gmail.com>
2022-11-19 15:55:28 -0500

    Added ApplicationUser Model

--- souschef.server/Program.cs
+++ souschef.server/Program.cs
@@ -1,3 +1,6 @@
+using SousChef.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<PostGresDBContext>(opt =>
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
+
 
 var app = builder.Build();
 
