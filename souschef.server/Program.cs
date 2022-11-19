9aa71b05c8c1f8a77154f0b549fdd17f3c722d2a
sethclim <sethclim@gmail.com>
2022-11-19 17:29:17 -0500

    Added User Controller

--- souschef.server/Program.cs
+++ souschef.server/Program.cs
@@ -1,9 +1,8 @@
 using SousChef.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,6 +12,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<PostGresDBContext>(opt =>
         opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
+builder.Services.AddIdentityCore<ApplicationUser>()
+                .AddEntityFrameworkStores<PostGresDBContext>();
+
 
 var app = builder.Build();
 
@@ -21,7 +23,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using(var scope = app.Services.CreateScope())
+    {
+        var salesContext = scope.ServiceProvider.GetRequiredService<PostGresDBContext>();
+        salesContext.Database.Migrate();
+        //salesContext.Seed();
+    }
 }
+
 
 app.UseHttpsRedirection();
 
